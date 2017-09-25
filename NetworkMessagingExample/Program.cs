d04d7234e7cdb4a273b093346272a4684ff84b13
Jason Cox <coxj@vt.edu>
2017-09-25 13:28:24 -0600

    Initial Commit

---
+++ NetworkMessagingExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace NetworkMessagingExample
+{
+	class MainClass
+	{
+		protected static TcpClient client = new TcpClient();
+		protected static readonly int PORT = 8888;
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the IP Address of the computer you wish to talk to:");
+			String IP = Console.ReadLine();
+
+			// Connect to the destination ip address.
+			client.Connect(IP, PORT);
+
+			Console.WriteLine("Connecting...");
+			while (!client.Connected)
+			{
+				// wait....
+			}
+
+			Console.WriteLine("Client is connected!");
+
+			Console.WriteLine("Enter a message to send.");
+			String message = Console.ReadLine();
+
+			byte[] messageData = Encoding.UTF8.GetBytes("^" + message + "$");
+
+			NetworkStream clientStream = client.GetStream();
+			clientStream.Write(messageData, 0, messageData.Length);
+			clientStream.Flush();
+
+			Console.WriteLine("Message Sent.");
+
+			Console.WriteLine("Awaiting Response...");
+
+			byte[] responseData = new byte[client.ReceiveBufferSize];
+			clientStream.Read(responseData, 0, client.ReceiveBufferSize);
+			String response = Encoding.UTF8.GetString(responseData);
+
+			Console.WriteLine("Remote sender says: {0}", response);
+			Console.ReadKey();
+			client.Close();
+		}
+	}
+}
---
+++ TcpMessageServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TcpMessageServer
+{
+	class MainClass
+	{
+		protected static readonly int PORT = 8888;
+		protected static TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), PORT);
+
+		protected static List<TcpClient> clientList = new List<TcpClient>();
+
+		public static void Main(string[] args)
+		{
+			server.Start();
+			Console.WriteLine("Server Started.");
+
+			while (true)
+			{
+				// This allows the server I'm running in this program to accept your connection
+				TcpClient client = server.AcceptTcpClient();
+
+				NetworkStream clientStream = client.GetStream();
+
+				byte[] responseData = new byte[client.ReceiveBufferSize];
+				clientStream.Read(responseData, 0, client.ReceiveBufferSize);
+				String response = Encoding.UTF8.GetString(responseData);
+
+				Console.WriteLine("New Client Connected.");
+				Console.WriteLine(response);
+				Console.Write(">  ");
+
+				String message = Console.ReadLine();
+
+				byte[] messageData = Encoding.UTF8.GetBytes("^" + message + "$");
+				clientStream.Write(messageData, 0, messageData.Length);
+
+				Console.WriteLine("Sent.");
+
+				clientList.Add(client);
+			}
+		}
+	}
+}
